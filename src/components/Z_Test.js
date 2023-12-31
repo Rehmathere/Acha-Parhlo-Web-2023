6c666e25edf272a0d2c127dc0b0104d490ecadb9
REHMAT QAZI <103884066+Rehmathere@users.noreply.github.com>
2023-12-31 22:06:21 +0500

    Add files via upload

--- src/components/Z_Test.js
+++ src/components/Z_Test.js
@@ -1,41 +1,63 @@
 // ChangePassword.js
 import React, { useState } from 'react';
-import { database } from './Portal/firebase'
-import 'firebase/auth';
+// CSS
+import './Portal/MyFeatures/PTracking/PT.css'
+// Images
+import fir from './Pics/Z_Call.png'
 
 export default function Z_Test() {
-  const [newPassword, setNewPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const handleChangePassword = async () => {
-    try {
-      const user = database.auth().currentUser;
-
-      if (user) {
-        await user.updatePassword(newPassword);
-        setErrorMessage('');
-      } else {
-        setErrorMessage('User not found. Please log in again.');
-      }
-    } catch (error) {
-      setErrorMessage(error.message);
-    }
+  // Status Logic
+  const [button1Color, setButton1Color] = useState('lightgray');
+  const [button2Color, setButton2Color] = useState('lightgray');
+  const [button3Color, setButton3Color] = useState('lightgray');
+  // Functions
+  // 1
+  const handleButton1Click = () => {
+    // Button 1
+    setButton1Color('green');
   };
-
+  // 2
+  const handleButton2Click = () => {
+    // Button 2
+    setButton2Color('green');
+    // Button 1 
+    setButton1Color('lightgreen');
+  };
+  // 3
+  const handleButton3Click = () => {
+    // Button 3
+    setButton3Color('green');
+    // Button 1 
+    setButton1Color('lightgreen');
+    // Button 2 
+    setButton2Color('lightgreen');
+  };
+  // Main Body
   return (
     <div>
-      <h2>Change Password</h2>
-      <div>
-        <label>New Password:</label>
-        <input
-          type="password"
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-        />
+      <h1>Application Tracking Setting</h1>
+      <br /><br />
+      {/* Box 1 */}
+      <div id="Z_box" style={{ backgroundColor: button1Color }}>
+        <img src={fir} alt="NA" />
       </div>
-      <button onClick={handleChangePassword}>Change Password</button>
-
-      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      <br />
+      <button onClick={handleButton1Click}>First Step</button>
+      <br /><br />
+      {/* Box 2 */}
+      <div id="Z_box" style={{ backgroundColor: button2Color }}>
+        <img src={fir} alt="NA" />
+      </div>
+      <br />
+      <button onClick={handleButton2Click}>First Step</button>
+      <br /><br />
+      {/* Box 3 */}
+      <div id="Z_box" style={{ backgroundColor: button3Color }}>
+        <img src={fir} alt="NA" />
+      </div>
+      <br />
+      <button onClick={handleButton3Click}>First Step</button>
+      <br />
     </div>
   );
 };
