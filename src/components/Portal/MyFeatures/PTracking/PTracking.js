a829f1547775830bf5c46dcadecf4c91a5299633
REHMAT QAZI <103884066+Rehmathere@users.noreply.github.com>
2024-01-03 00:12:43 +0500

    Add files via upload

--- src/components/Portal/MyFeatures/PTracking/PTracking.js
+++ src/components/Portal/MyFeatures/PTracking/PTracking.js
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom'
 import '../PProfile/PProfile.css'
 // Images
 import logout from '../../../Pics/logout.png'
+import Ptr from "../../../Pics/PTr.png"
 
 export default function PTracking() {
     // useNavigate
@@ -28,13 +29,72 @@ export default function PTracking() {
             </div>
             {/* ------------- */}
             <PSidebar>
-                <h1>Portal Tracking</h1>
-                <br />
-                <button onClick={() => navigate('/PTStatus')}>Check PTStatus</button>
+                <>
+                    {/* 1 - Heading */}
+                    <div id="PTr_first">
+                        <div id="sub_PTr_first">
+                            <div id="PTr_first_1">
+                                <h1>Application Tracking</h1>
+                            </div>
+                            <div id="PTr_first_2">
+                                <img src={Ptr} alt="Na" />
+                            </div>
+                        </div>
+                    </div>
+                    {/* 2 - Ptr_Second */}
+                    <div id="PTr_second">
+                        <div id="sub_PTr_second">
+                            {/* 2 - Part 1 */}
+                            <div id="PTr_second_Part1">
+                                <div id="PTr_second_Part1_Box">
+                                    <div id="PTr_second_Part1_1">
+                                        Sr No.
+                                    </div>
+                                    <div id="PTr_second_Part1_2">
+                                        Name
+                                    </div>
+                                    <div id="PTr_second_Part1_3">
+                                        University
+                                    </div>
+                                    <div id="PTr_second_Part1_4">
+                                        Course
+                                    </div>
+                                </div>
+                            </div>
+                            {/* 2 - Part 2 */}
+                            <div id="PTr_second_Part2">
+                                {/* Box2 Number 1 */}
+                                <div id="PTr_sec_Part2_Box" onClick={() => navigate('/PTStatus')}>
+                                    <div id="PTr_sec_Part2_1">
+                                        1
+                                    </div>
+                                    <div id="PTr_sec_Part2_2">
+                                        Qazi Rehmat Hussain
+                                    </div>
+                                    <div id="PTr_sec_Part2_3">
+                                        The Western Sdyney
+                                    </div>
+                                    <div id="PTr_sec_Part2_4">
+                                        Apllied Science
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* Start */}
+                        </div>
+                    </div>
+                    {/* --------------------------------------------- */}
+                    {/* <br />
+                    <br />
+                    <button onClick={() => navigate('/PTStatus')}>Check PTStatus</button> */}
+                </>
             </PSidebar>
         </div>
     )
 }
+
+
+
 
 
 
